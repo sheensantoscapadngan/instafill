178b9636aed3a2e74dc9a785ae50c4acd2c98569
sheensantoscapadngan <sheensantoscapadngan@gmail.com>
2021-02-21 17:38:57 +0800

    Edited React and Flask Config

--- api/app.py
+++ api/app.py
@@ -1,9 +1,8 @@
 import time
-from flask import Flask
+from flask import Flask,jsonify #import jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    return str(time.time())
