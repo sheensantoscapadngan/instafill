4367e59c63a2399d909931d4e66de47e90725a0c
Andry <ajtajaja13@gmail.com>
2021-03-10 23:23:10 +0800

    merged
    successful merge!

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './index.css';
+import './tooltip.css';
 import {Button, Popup, Dropzone } from '../components/common';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -9,6 +10,22 @@ import  { useState } from 'react';
 
 library.add(faUpload, faPen)
 
+//messages for popup
+function FirstButton(){
+  return(
+    "Upload your blank pdf files here for instant fill up of information! Don't forget to upload your master file as well. Sit back and we'll do the rest."
+  );
+};
+function SecondButton(){
+  return(
+    "Upload your master document here! The master document will be used for the instant fill up.\nDon't have a master document yet? You can also create your own now!"
+  );
+};
+function ThirdButton(){
+  return(
+    "Start creating your own master document here! The master document makes the magic work. It's completely quick and easy!"
+  );
+};
 const MainPage = () =>{
 
 
@@ -25,8 +42,10 @@ const MainPage = () =>{
           
         
           <div className="buttons">
-
-            <Button
+            <view 
+              className = "stylish"
+              data-tooltip = {FirstButton()}> 
+              <Button
               type="button"
               buttonStyle="btn--primary--solid"
               buttonSize="btn--large"
@@ -35,7 +54,11 @@ const MainPage = () =>{
               <p className="biggerText">Upload file</p>
               <p className="smallerText">Upload your blank pdf files here!</p>
             </Button>
+            </view>
 
+            <view
+            className = "stylish"
+            data-tooltip = {SecondButton()}>
             <Button
               type="button"
               buttonStyle="btn--primary--solid"
@@ -46,16 +69,23 @@ const MainPage = () =>{
               <p className="smallerText">Upload your master document here!</p>
             </Button>
 
-            <Link to='/createMaster'>
-              <Button 
-                type="button"
-                buttonStyle="btn--primary--solid"
-                buttonSize="btn--large">
-                <FontAwesomeIcon icon="pen"/>
-                <p className="biggerText">Create Master</p>
-                <p className="smallerText">Create your master document here!</p>
-              </Button>
-            </Link>
+            </view>
+            
+
+            <view 
+              className = "stylish"
+              data-tooltip = {ThirdButton()}>
+              <Link to='/createMaster'>
+                <Button 
+                  type="button"
+                  buttonStyle="btn--primary--solid"
+                  buttonSize="btn--large">
+                  <FontAwesomeIcon icon="pen"/>
+                  <p className="biggerText">Create Master</p>
+                  <p className="smallerText">Create your master document here!</p>
+                </Button>
+              </Link>
+            </view>
           </div>
         </div>
     )
