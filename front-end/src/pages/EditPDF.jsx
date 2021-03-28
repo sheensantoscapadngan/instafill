f58f206e443f6e33e91b9959fe1acb8e6a73b7fd
Joseph Duron Jr <djesph@gmail.com>
2021-03-29 01:01:24 +0800

    cleaned EditPDF

--- front-end/src/pages/EditPDF.jsx
+++ front-end/src/pages/EditPDF.jsx
@@ -1,19 +1,12 @@
 import {Navbar} from '../components/common';
 import React, { useState } from 'react';
-
-
-import {DropzonePDF} from '../components/common';
 import './EditPDF.css';
-
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const EditPDF = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [file, setFile] = useState([]);
-  const [fileUploadPDFs, setFileUploadPDFs] = useState();
-
-
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
@@ -23,22 +16,15 @@ const EditPDF = () => {
     <div >
 
         <Navbar/>
-        <div className="dont-display">
-        <div className="editpdf">
-            <DropzonePDF setFileUploadPDFs={setFileUploadPDFs}/> 
-            </div>
       <Document
-        file={fileUploadPDFs}
+        file="sample.pdf"
         onLoadSuccess={onDocumentLoadSuccess}
       >
          <Page pageNumber={pageNumber} />
       </Document>
-      </div>
-     
+ 
       <p>Page {pageNumber} of {numPages}</p>
-      <p>Page {pageNumber} of {numPages}</p>
-      <p>Page {pageNumber} of {numPages}</p>
-      <h1>yolo</h1>
+
     </div>
   );
 }
